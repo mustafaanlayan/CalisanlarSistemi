e3a2e1b3ae8253c78cdd5b8bdbac2f1b33dac147
mustafaanlayan <32699486+mustafaanlayan@users.noreply.github.com>
2021-12-02 15:31:35 +0300

    commit database olusturma

---
+++ CalisanPersonelBilgi/CalisanBilgi.Data/DataContext/CalisanBilgiContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace CalisanBilgi.Data.DataContext
+{
+   public class CalisanBilgiContext: IdentityDbContext
+    {
+        public CalisanBilgiContext(DbContextOptions options):base(options)
+        {
+            
+        }
+    }
+}
